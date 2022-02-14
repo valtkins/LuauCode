d3c66df195215203ba0548c7a69daad7b24c0612
valtkins <60578301+valtkins@users.noreply.github.com>
2022-02-14 14:34:01 +0100

    Update SchoolScheduleServer.lua

--- SchoolScheduleServer.lua
+++ SchoolScheduleServer.lua
@@ -1,18 +1,14 @@
 local module = {}
 
 function module:init()
-
 	-- have a list of classes
 	-- randomize them MON-FRI
-
 	-- announce class started
 	-- teleport button
 	-- start minigame
-
 	-- display grade, do rewards!
 
 	local SchoolMinigames = require(script.Parent.SchoolMinigames)
-
 	local amountClasses = 5
 	local classList = {}
 
@@ -25,13 +21,10 @@ function module:init()
 	}
 
 	local function setUpClasses()
-
 		-- so lets do... 8,9,10,11 AM and 1PM class (12PM-1PM lunch)
 		classList = {}
 		local day = workspace:GetAttribute("Day") 
-
 		if day ~= "SUN" and day ~= "SAT" then
-
 			local existingList = {}
 			for _, class in pairs(classes) do
 				table.insert(existingList, class)
@@ -42,18 +35,15 @@ function module:init()
 				table.insert(classList,existingList[chosen])
 				table.remove(existingList,chosen)
 			end
-
 			-- classList is the randomized classes for the day! :))
-
 		end
 	end
-
 	local function changeClass()
 		workspace:SetAttribute("NextClass", "None")
 		if #classList >= 1 then
 			local hour = workspace:GetAttribute("MilitaryHour")
 			local classTimes = { 8,9,10,11,13 } -- 8am, 9am, 10am, 11am, 1pm classes
-
+			
 			local pos = table.find(classTimes, hour)
 			if pos then
 				if classList[pos+1] then
@@ -65,9 +55,6 @@ function module:init()
 				workspace:SetAttribute('ClassName', chosenClass)
 				SchoolMinigames:startMinigame(chosenClass)
 				-- display the class for users!
-
-
-
 			elseif hour == 12 then
 				workspace:SetAttribute("ClassName","Lunch")
 				local pos = table.find(classTimes, hour+1)
@@ -84,7 +71,6 @@ function module:init()
 			workspace:SetAttribute("ClassName","Free Time")
 		end
 	end
-	
 	if workspace:GetAttribute("Day") then
 		workspace:GetAttributeChangedSignal("Day"):Connect(function()
 			setUpClasses()
@@ -92,16 +78,11 @@ function module:init()
 		workspace:GetAttributeChangedSignal("MilitaryHour"):Connect(function()
 			changeClass()
 		end)
-
-		
 		setUpClasses()
 		changeClass()
 	else
 		warn("Workspace has no attribute 'Day' check DayNightCycle")
 	end
-
-
 end
-
 
 return module
